dca248ec620a5df5a5ade07649c392820b8926c0
dittmanns <dittmanns@ergovia.de>
2019-07-17 14:32:58 +0200

    JS Changes

--- javascript/login-handler.js
+++ javascript/login-handler.js
@@ -1,50 +1,34 @@
 'use strict';
-console.log(this);
 
 let movePlaceholder = function() {
-    let fieldUser = document.getElementsByClassName('input');
-    let placeholderUser = document.querySelector('.label');
+    let fieldUser = document.querySelector('.username input');
+    let placeholderUser = document.querySelector('.username label');
     
-    console.log(fieldUser);
-    console.log(placeholderUser);
-    /*
     let fieldPassword = document.querySelector('.password input');
     let placeholderPassword = document.querySelector('.password label');
-    */
-    for(var i = 0; i < fieldUser.length; i++){
 
-        fieldUser[i].onfocus = function() {
-            this.classList.add('placeholder');
-        }; 
-        
-        fieldUser[i].onblur = function() {
-            this.classList.remove('placeholder');
-        };
-        
-        fieldUser[i].addEventListener('change', function() {
-            if(this.value.length != null) {
-                this.classList.add('writing');
-            } else {
-                this[i].classList.remove('writing');
-            }
-        });
-    }
+    fieldUser.onfocus = function() {
+        placeholderUser.classList.add('placeholder');
+    }; 
+
+    fieldUser.onblur = function() {
+        placeholderUser.classList.remove('placeholder');
+    };
     
-    /*
+    fieldUser.addEventListener('change', function() {
+        if(fieldUser.value !== '') {
+            placeholderUser.classList.add('writing');
+        } else {
+            placeholderUser.classList.remove('writing');
+        }
+    })
+    
     fieldPassword.onfocus = function() {
         placeholderPassword.classList.add('placeholder');
     }; 
     fieldPassword.onblur = function() {
         placeholderPassword.classList.remove('placeholder');
     };
-    fieldPassword.addEventListener('change', function() {
-        if(fieldPassword.value.length != null) {
-            placeholderPassword.classList.add('writing');
-        } else {
-            placeholderPassword.classList.remove('writing');
-        }
-    });
-    */
 };
 
 movePlaceholder();
